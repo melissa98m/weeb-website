f7f87736cc2a3f0909acc89d7e926c2717824d61
melissa98m <melissa.mangione@gmail.com>
2025-05-20 14:35:20 +0200

    feat: create contact components

---
+++ src/components/Contact/ContactIntro.jsx
@@ -0,0 +1,15 @@
+export default function ContactIntro() {
+  return (
+    <section className="text-center px-6 py-16 max-w-3xl mx-auto">
+      <h1 className="text-7xl md:text-4xl font-bold mb-4">
+        Votre avis compte !
+      </h1>
+      <p className="text-white text-sm md:text-base">
+        Le monde du web évolue constamment, et nous sommes là pour vous guider à
+        travers ses tendances, technologies et meilleures pratiques. Que vous
+        soyez développeur, designer ou passionné du digital, notre blog vous
+        offre du contenu de qualité pour rester à la pointe.
+      </p>
+    </section>
+  );
+}
