2a359c0d94613ff168af132ae6dbcd0c267c5c7d
melissa98m <melissa.mangione@gmail.com>
2025-05-20 16:57:35 +0200

    style: delete opacity in block trustedby

--- src/components/Home/TrustedBy.jsx
+++ src/components/Home/TrustedBy.jsx
@@ -2,7 +2,7 @@ export default function TrustedBy() {
   return (
     <section className="text-center py-12">
       <h2 className="text-6xl font-extrabold mb-6">Ils nous font confiance</h2>
-      <div className="flex flex-wrap justify-center items-center gap-8 opacity-80">
+      <div className="flex flex-wrap justify-center items-center gap-8">
         <img
           src="src\assets\home\smartfinder.svg"
           alt="SmartFinder"
