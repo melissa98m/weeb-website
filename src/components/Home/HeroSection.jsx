68c7c74ca20d919163667e42cc9e2181db0d5c69
melissa98m <melissa.mangione@gmail.com>
2025-05-21 19:22:52 +0200

    feat: add animation img to home

--- src/components/Home/HeroSection.jsx
+++ src/components/Home/HeroSection.jsx
@@ -1,8 +1,11 @@
 import { useTheme} from "../../context/ThemeContext"
+import { motion } from 'framer-motion';
+
 export default function HeroSection() {
   const { theme } = useTheme();
   return (
     <section className="px-6 py-16 text-center max-w-5xl mx-auto">
+     
       <h1 className="text-7xl md:text-5xl font-bold leading-tight">
         Explorez le <span className={`${theme === 'dark' ? 'text-primary' : 'text-secondary'}`}>Web</span> sous toutes ses 
         <span className={`underline underline-offset-8 ${theme === 'dark' ?  'decoration-primary' : 'decoration-secondary'}`}> facettes</span>
@@ -21,10 +24,19 @@ export default function HeroSection() {
           S’abonner à la newsletter
         </button>
       </div>
-      <img
-        src="src\assets\home\mokup.png"
+      <motion.img
+        src="src/assets/home/mokup.png"
         alt="Mockup"
         className="mt-12 w-full max-w-5xl mx-auto rounded-md shadow-lg"
+        style={{
+          cursor: 'grab',
+        }}
+        drag
+        dragDirectionLock
+        dragConstraints={{ top: 0, right: 0, bottom: 0, left: 0 }}
+        dragTransition={{ bounceStiffness: 600, bounceDamping: 20 }}
+        dragElastic={0.5}
+        whileTap={{ cursor: 'grabbing' }}
       />
     </section>
   );
