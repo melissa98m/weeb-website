d5fbeb86090b202cd655bc5005e9592de9b5303d
melissa98m <melissa.mangione@gmail.com>
2025-05-21 11:01:15 +0200

    style: style theme light/dark component LearningSection

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,10 +2,14 @@ import HeroSection from "../components/Home/HeroSection";
 import TrustedBy from "../components/Home/TrustedBy";
 import LearningSection from "../components/Home/LearningSection";
 import TrendsSection from "../components/Home/TrendsSection";
+import { useTheme } from "../context/ThemeContext";
+
 
 export default function Home() {
+
+  const { theme } = useTheme();
   return (
-    <main className="bg-background text-white">
+    <main className={`${theme === 'dark' ? 'bg-background text-white' : 'bg-light text-dark'}`}>
       <HeroSection />
       <TrustedBy />
       <LearningSection />
