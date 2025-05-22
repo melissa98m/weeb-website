5bf2266bca112360efe73478d328ba684e1241c0
melissa98m <melissa.mangione@gmail.com>
2025-05-22 15:42:18 +0200

    feat: traduction contact  fr + en

--- src/components/Contact/ContactIntro.jsx
+++ src/components/Contact/ContactIntro.jsx
@@ -1,14 +1,16 @@
+import contactEn from "../../../locales/en/contact.json";
+import contactFr from "../../../locales/fr/contact.json";
+import { useLanguage } from "../../context/LanguageContext";
+
 export default function ContactIntro() {
+  const { language } = useLanguage();
   return (
     <section className="text-center px-6 py-16 max-w-3xl mx-auto">
       <h1 className="text-7xl md:text-4xl font-bold mb-4">
-        Votre avis compte !
+        {language === "fr" ? contactFr.contact_title : contactEn.contact_title}
       </h1>
       <p className="text-sm md:text-base">
-        Le monde du web évolue constamment, et nous sommes là pour vous guider à
-        travers ses tendances, technologies et meilleures pratiques. Que vous
-        soyez développeur, designer ou passionné du digital, notre blog vous
-        offre du contenu de qualité pour rester à la pointe.
+        {language === "fr" ? contactFr.contact_intro : contactEn.contact_intro}
       </p>
     </section>
   );
