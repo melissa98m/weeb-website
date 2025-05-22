67feb50c2f8f34daa51a224347afb2074c6ec958
melissa98m <melissa.mangione@gmail.com>
2025-05-22 16:26:11 +0200

    fix: change btn to link on home page

--- src/components/Home/HeroSection.jsx
+++ src/components/Home/HeroSection.jsx
@@ -3,6 +3,7 @@ import { motion } from "framer-motion";
 import homeEn from "../../../locales/en/home.json";
 import homeFr from "../../../locales/fr/home.json";
 import { useLanguage } from "../../context/LanguageContext";
+import { Link } from "react-router-dom";
 
 export default function HeroSection() {
   const { theme } = useTheme();
@@ -34,7 +35,8 @@ export default function HeroSection() {
         {language === "fr" ? homeFr.web_world : homeEn.web_world}
       </p>
       <div className="mt-6 flex flex-col sm:flex-row gap-4 justify-center">
-        <button
+        <Link
+          to="/articles"
           className={`px-6 py-2 rounded-md shadow  ${
             theme === "dark"
               ? "bg-secondary text-white"
@@ -42,14 +44,15 @@ export default function HeroSection() {
           }`}
         >
           {language === "fr" ? homeFr.link_1 : homeEn.link_1}
-        </button>
-        <button
+        </Link>
+        <Link
+          to="/newsletter"
           className={`border px-6 py-2 rounded-md ${
             theme === "dark" ? "border-white" : "border-dark"
           }`}
         >
           {language === "fr" ? homeFr.newsletter : homeEn.newsletter}
-        </button>
+        </Link>
       </div>
       <motion.img
         src="/home/mokup.png"
