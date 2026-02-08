c087302c29ff7afb1d4d5ce1d7114ff8ccc27fa3
melissa98m <melissa.mangione@gmail.com>
2026-02-08 11:33:00 +0100

    add sentry for log error

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import * as Sentry from "@sentry/react";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
@@ -7,16 +8,53 @@ import { ThemeProvider } from "./context/ThemeContext";
 import { LanguageProvider } from "./context/LanguageContext.jsx";
 import { AuthProvider } from "./context/AuthContext.jsx";
 
+const sentryDsn = import.meta.env.VITE_SENTRY_DSN;
+const sentryEnabled = import.meta.env.PROD && Boolean(sentryDsn);
+
+if (sentryEnabled) {
+  const integrations = [];
+
+  if (typeof Sentry.browserTracingIntegration === "function") {
+    integrations.push(Sentry.browserTracingIntegration());
+  } else if (typeof Sentry.browserTracing === "function") {
+    integrations.push(Sentry.browserTracing());
+  }
+
+  if (typeof Sentry.replayIntegration === "function") {
+    integrations.push(Sentry.replayIntegration());
+  } else if (typeof Sentry.replay === "function") {
+    integrations.push(Sentry.replay());
+  }
+
+  Sentry.init({
+    dsn: sentryDsn,
+    integrations,
+    traces_sample_rate: 1.0,
+    replays_session_sample_rate: 0.1,
+    replays_on_error_sample_rate: 1.0,
+  });
+}
+
+const appTree = (
+  <BrowserRouter>
+    <ThemeProvider>
+      <LanguageProvider>
+        <AuthProvider>
+          <App />
+        </AuthProvider>
+      </LanguageProvider>
+    </ThemeProvider>
+  </BrowserRouter>
+);
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
-      <ThemeProvider>
-        <LanguageProvider>
-          <AuthProvider>
-            <App />
-          </AuthProvider>
-        </LanguageProvider>
-      </ThemeProvider>
-    </BrowserRouter>
+    {sentryEnabled ? (
+      <Sentry.ErrorBoundary fallback={<div>Une erreur est survenue.</div>}>
+        {appTree}
+      </Sentry.ErrorBoundary>
+    ) : (
+      appTree
+    )}
   </StrictMode>
 );
