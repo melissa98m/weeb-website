8b587763d72f092cd65096e3fa430d58ef807fe2
melissa98m <melissa.mangione@gmail.com>
2026-02-12 08:56:14 +0100

    Align reset and newsletter payloads with backend contract

--- src/pages/ResetPassword.test.jsx
+++ src/pages/ResetPassword.test.jsx
@@ -65,7 +65,6 @@ describe("ResetPassword", () => {
       uid: "abc",
       token: "def",
       password: "Abcd1234!",
-      password_confirm: "Abcd1234!",
     });
 
     expect(await screen.findByText(resetEn.success_message)).toBeInTheDocument();
